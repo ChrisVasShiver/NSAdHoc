47310e9e7991deac5bd93354b7f011c55f95de03
ChrisVasShiver <c_visscher@hotmail.com>
2016-04-06 13:20:09 +0200

    beginning of handeling packets

--- src/threads/UniListeningThread.java
+++ src/threads/UniListeningThread.java
@@ -1,5 +1,11 @@
 package threads;
 
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import helper.Packet;
+
 public class UniListeningThread implements Runnable {
 
 	public volatile boolean wait = true;
@@ -13,5 +19,21 @@ public class UniListeningThread implements Runnable {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	public void handlePacket(byte[] raw) throws UnknownHostException {
+		Packet packet = new Packet(raw);
+		if(packet.getDest() == InetAddress.getLocalHost()) {
+			//TODO what if the packet is for me
+			Packet ackpkt = new Packet(InetAddress.getLocalHost(), packet.getDest(), 0,0,0, null);
+			//TODO add destination address
+			DatagramPacket pkt = new DatagramPacket(ackpkt.getBytes(), ackpkt.getBytes().length);
+		} else {
+			if(!packet.isExpired()) {
+				packet.decreaseTTL();
+				//TODO what if the packet is not expired.
+				//TODO add destination address
+				DatagramPacket pkt = new DatagramPacket(packet.getBytes(), packet.getBytes().length);
+			}
+		}
+	}
 }
